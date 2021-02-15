da1fc8fa941635ed3ddc49a2da4faf03675ee42f
whistlepark <arhanna@ucsd.edu>
2021-02-15 10:50:39 -0800

    need to update models

--- WebInterface/server/src/manage.py
+++ WebInterface/server/src/manage.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
 import sys
-
+import os
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-    if "BEARER_TOKEN" in os.environ:
-        bearer = os.environ["BEARER_TOKEN"]
-    else:
-        sys.exit("Missing BEARER_TOKEN env variable")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- WebInterface/server/src/user/models.py
+++ WebInterface/server/src/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class TwitterUser(models.Model):
+    username = models.CharField(max_length=100)
+    user_id  = models.IntegerField(primary_key=True, max_length=100)
+    
+    def __str__(self):
+        return self.username
+
+class UserTweet(models.Model):
+    """
+        Tweets from a specific user
+    """
+
+    message = models.CharField(max_length=1000)
+    user = models.ForeignKey('TwitterUser', on_delete=models.RESTRICT)
+    tweet_id = models.IntegerField(primary_key=True)
+
