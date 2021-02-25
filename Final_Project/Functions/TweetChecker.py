c88d533cbf7f1efb788d227ae1b2ff40a1090ef5
Chris <cdlight@ucsd.edu>
2021-02-24 17:40:56 -0800

    TweetChecker.py implemented

--- Final_Project/Functions/TweetChecker.py
+++ Final_Project/Functions/TweetChecker.py
@@ -18,7 +18,33 @@ CountVect_Vect = pickle.load(open('count_vectorizer.pickle', 'rb'))
 
 print('Tweet checker. Test the positivity of your tweet (before sending it!)')
 
-userText = input('Type your tweet to check or \'end\' if you\'re done')
+userText = input('Type your tweet (up to 280 characters) to check positivity or \'end\' if you\'re done: ')
 while userText != 'end':
+    assert len(userText)<280
+    if input('Select the model to use: Tf-Idf (1) or Count Vectorizer (2): ') == '1':
+        model = TfIdf_Model
+        vectorizer = TfIdf_Vectorizer
+        modelLabel = 'Tf-Idf'
+    else:
+        model = CountVect_Model
+        vectorizer = CountVect_Vect
+        modelLabel = 'Count Vectorizer'
+    tweetObj = MF.predict([userText], vectorizer, model, silence=True)
 
-    userText = input('Type your tweet to check or \'end\' if you\'re done')
+    tweetColor = np.ones((3,3))*tweetObj[0][2]
+    tweetColor = tweetColor if tweetObj[0][1] == 'Positive' else -1*tweetColor
+    tweetColor = (tweetColor+1)/2
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(tweetColor, cmap=cm.get_cmap('RdYlGn'), vmax=1.0, vmin=0.0)
+    ax.axis('off')
+    if len(userText)>20:
+        fontSize=15
+    else:
+        fontSize=-3/(2*len(userText)) + 30
+    plt.text(1, 1, userText,
+              bbox={'facecolor': 'beige', 'alpha': 1, 'edgecolor': 'none', 'pad': 1},
+              ha='center', va='center', fontsize=int(fontSize))
+    plt.text(1,2, 'Your tweet classifies as: ' + tweetObj[0][1], ha='center',va='center', fontsize=15)
+    plt.show()
+    userText = input('Type your tweet (up to 280 characters) to check positivity or \'end\' if you\'re done: ')
